a994a52fe9894a5f36a3085c5cf5c9f1aa756f13
sameerpanda76 <sameer.panda76@gmail.com>
2025-11-05 17:16:55 +0100

    Make postInstall.sh executable

--- postInstall.sh
+++ postInstall.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "🔧 Building CmdStan backend for Prophet..."
 pip install --upgrade pip
 pip install cmdstanpy
 python - << 'EOF'
 import cmdstanpy
+print("CmdStan build started…")
 cmdstanpy.install_cmdstan()
+print("✅ CmdStan build finished.")
 EOF
