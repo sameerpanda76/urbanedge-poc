db273ed8c66361db1e6847524a5c824cb02019a2
sameerpanda76 <sameer.panda76@gmail.com>
2025-11-05 16:18:13 +0100

    Updated postinstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo "🔧 Building CmdStan backend for Prophet..."
-python - <<EOF
+pip install --upgrade pip
+pip install cmdstanpy
+python - << 'EOF'
 import cmdstanpy
-print("CmdStan build started…")
 cmdstanpy.install_cmdstan()
-print("✅ CmdStan build finished.")
 EOF
