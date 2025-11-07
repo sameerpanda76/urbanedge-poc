c3e73df28c8150b6e6765def71fbdd2c2d11b415
sameerpanda76 <sameer.panda76@gmail.com>
2025-11-07 14:46:07 +0100

    Uodated req. and urbanedge

--- src/urbanedge_app.py
+++ src/urbanedge_app.py
@@ -3,7 +3,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import prophet.models
 prophet.models._is_cmdstanpy_installed = lambda: False
-prophet.models._get_backend = lambda *args, **kwargs: None  
 
 import streamlit as st
 import pandas as pd
